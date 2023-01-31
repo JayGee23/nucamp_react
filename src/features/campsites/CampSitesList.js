192f6792e59b65f59867a48bc42dc3dacabc9091
Jay Gee <jayguarino@gmail.com>
2023-01-30 20:11:50 -0800

    error and loading

--- src/features/campsites/CampSitesList.js
+++ src/features/campsites/CampSitesList.js
@@ -2,20 +2,41 @@ import { useSelector } from "react-redux";
 import CampsiteCard from "./CampsiteCard";
 import { Col, Row } from 'reactstrap';
 import { selectAllCampsites } from './campsitesSlice';
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 
 const CampSitesList = () => {
     const campsites = useSelector(selectAllCampsites);
     console.log('campsites:', campsites);
 
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg)
+
+    if (isLoading) {
+        return (
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if (errMsg) {
+        return (
+            <Row>
+                <Error errMsg={errMsg}/>
+            </Row>
+        )
+    }
+
     return (
         <Row className='ms-auto'>
-            {campsites.map((campsite)=> {
+            {campsites.map((campsite) => {
                 return (
-                    <Col 
-                        md='5' 
-                        className='m-4' 
+                    <Col
+                        md='5'
+                        className='m-4'
                         key={campsite.id}
-                        >
+                    >
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 )
